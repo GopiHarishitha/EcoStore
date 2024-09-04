aefb805d3ef5b23f75083d9bb3636559653d838b
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-09-04 14:32:08 +0530

    Initialized Backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.send({ message: "Test Successful" }));
 
+const port = 7000;
 
-app.listen(port, ()=>{console.log(`port listening at ${port}`)})
+app.listen(port, () => console.log(`server listening at ${port}`));
