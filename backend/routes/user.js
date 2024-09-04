dc2e21313cec7b3a86327bc28169e9fc4a6578da
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-09-04 16:40:09 +0530

    Finished User authentication

---
+++ backend/routes/user.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { createUser, loginUser } = require("../db/user");
+const router = express.Router();
+require("dotenv").config();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    const user = await createUser(username, password, email);
+    res.send({ success: true, message: "Registered Successfully", user: user });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "Registration failed: " + error.message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await loginUser(username, password);
+
+    const token = jwt.sign({ username: username }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    res.send({
+      success: true,
+      message: "Login Success",
+      user: user,
+      token: token,
+    });
+  } catch (error) {
+    res.send({ success: false, message: "Login failed: " + error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const userRouter = require("./routes/user");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database connnection established"))
+  .catch(() => console.log("Failed to connect to database"));
+
+app.use(express.json());
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => res.send({ message: "Test Successful" }));
 
-const port = 7000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`server listening at ${port}`));
