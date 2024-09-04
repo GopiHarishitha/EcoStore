92462eac83e48bd6e6729b93872ff2e498a968bd
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-09-05 04:02:40 +0530

    Created Recommendation System

---
+++ backend/routes/recommendation.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const redis = require("redis");
+const raccoon = require("raccoon");
+
+const redisClient = redis.createClient();
+
+raccoon.config.nearestNeighbors = 5;
+raccoon.config.className = "product";
+
+router.post("/like", async (req, res) => {
+  const { userId, productId } = req.body;
+
+  try {
+    await raccoon.liked(userId, productId);
+    res.send({ success: true, message: "Liked!" });
+  } catch (error) {
+    res.send({ success: false, message: "Failed to like" });
+  }
+});
+
+router.post("/dislike", async (req, res) => {
+  const { userId, productId } = req.body;
+
+  try {
+    await raccoon.disliked(userId, productId);
+    res.send({ success: true, message: "Disiked!" });
+  } catch (error) {
+    res.send({ success: false, message: "Failed to dislike" });
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const recommendations = await raccoon.recommendFor(userId, 8);
+    res.send({
+      message: "10 recommendations",
+      success: true,
+      recommendations: recommendations,
+    });
+  } catch (error) {
+    res.send({ success: false, message: "Failed to get recommendations" });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const userRouter = require("./routes/user");
+const recommendationRouter = require("./routes/recommendation");
 require("dotenv").config();
 
 mongoose
@@ -12,6 +13,7 @@ mongoose
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use("/recommendations", recommendationRouter);
 
 app.get("/", (_, res) => res.send({ message: "Test Successful" }));
 
